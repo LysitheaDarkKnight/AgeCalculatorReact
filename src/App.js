e2f50f3846e15fea8a62f8caa563976c04312769
moom1 <48647021+moom1@users.noreply.github.com>
2023-05-01 01:15:05 +0300

    refactor to properly pass data to ageDisplay comp

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@
 // import './App.css';
 import Form from "./components/Form";
 import AgeDisplay from "./components/AgeDisplay";
+import { useState } from "react"
+
 // import Input from "./components/Input";
 
 function App() {
+  const [day, setDay] = useState('')
+  const [month, setMonth] = useState('')
+  const [year, setYear] = useState('')
+  const [showAge, setShowAge] = useState(false)
+
+
   return (
     <main>
       <div className="container">
-        <Form>
+        <Form day={day} setDay={setDay} month={month} setMonth={setMonth} year={year} setYear={setYear} setShowAge={setShowAge}>
         </Form>
+
+        {showAge && <AgeDisplay day={day} month={month} year={year}/>}
+        
+       
       </div>
     </main>
   );
--- src/components/AgeDisplay.jsx
+++ src/components/AgeDisplay.jsx
@@ -1,28 +1,23 @@
-import { useState } from "react"
 
-function AgeDisplay() {
-  const [yearDisplay, setYearDisplay] = useState('--')
-  const [monthDisplay, setMonthDisplay] = useState('--')
-  const [dayDisplay, setDayDisplay] = useState('--')
-
+function AgeDisplay({day,month,year}) {
 
   return (
     <div className="age-display">
         <div className="age-display__number">
             {/* <input type="text" id="age-years__display" name="age-years__display" value={yearDisplay} disabled/>
             <label htmlFor="age-years__display">years</label> */}
-            <h1 id="age-years"><span id="age-years__calculate">{yearDisplay}</span> years</h1>
+            <h1 id="age-years"><span id="age-years__calculate">{year}</span> years</h1>
 
         </div>
         <div className="age-display__number">
             {/* <input type="text" id="age-months__display" name="age-months__display" value={monthDisplay} disabled/>
             <label htmlFor="age-months__display">months</label> */}
-            <h1 id="age-months"><span id="age-months__calculate">{monthDisplay}</span> months</h1>
+            <h1 id="age-months"><span id="age-months__calculate">{month}</span> months</h1>
         </div>
         <div className="age-display__number">
             {/* <input type="text" id="age-days__display" name="age-days__display" value={dayDisplay} disabled/>
             <label htmlFor="age-days__display">days</label> */}
-            <h1 id="age-days"><span id="age-days__calculate">{dayDisplay}</span> days</h1>
+            <h1 id="age-days"><span id="age-days__calculate">{day}</span> days</h1>
         </div>
 
     </div>
